c0638cde556e13c2c8b6415898a1fbe0509a7291
Hesam Rad <hesamzakerirad@gmail.com>
2025-07-01 09:18:36 +0330

    feat: add test suite

---
+++ tests/OtpTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HesamRad\Otp\Tests;
+
+class OtpTest extends TestCase
+{
+    public function test_it_returns_true()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace HesamRad\Otp\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    /**
+     * Setting up our testcase.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+}
