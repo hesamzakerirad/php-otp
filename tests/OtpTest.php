cde8d9b7046955e566169bd11b1274a173522d7d
Hesam Rad <hesamzakerirad@gmail.com>
2025-07-01 09:22:23 +0330

    feat: add tests

--- tests/OtpTest.php
+++ tests/OtpTest.php
@@ -4,8 +4,27 @@ namespace HesamRad\Otp\Tests;
 
 class OtpTest extends TestCase
 {
-    public function test_it_returns_true()
+    public function test_totp_is_generated()
     {
-        $this->assertTrue(true);
+        $totp = new \HesamRad\Otp\Totp(
+            '01010101',
+            0,
+        );
+
+        $otp = $totp->generate();
+
+        return $this->assertNotEmpty($otp);
+    }
+
+    public function test_hotp_is_generated()
+    {
+        $hotp = new \HesamRad\Otp\Hotp(
+            '01010101',
+            0,
+        );
+
+        $otp = $hotp->generate();
+
+        return $this->assertNotEmpty($otp);
     }
 }
